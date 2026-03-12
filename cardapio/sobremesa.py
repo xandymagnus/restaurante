1f06f395607883f69a2a4b8d653872d0cbc618ef
xandymagnus <xandy.magno325@gmail.com>
2026-03-12 16:41:34 -0300

    criando classe sobremesa e implementando as mesmas funcionalidades de classe bebida e prato, porém com outros atributos

---
+++ cardapio/sobremesa.py
@@ -0,0 +1,15 @@
+from item_cardapio import ItemCardapio
+
+class sobremesa(ItemCardapio):
+    def __init__(self, nome, preco, tipo, tamanho, descricao):
+        super().__init__(nome, preco)
+        
+        self._tipo = tipo
+        self.tamanho = tamanho
+        self.descricao = descricao
+        
+    def __str__(self):
+        return f'Nome: {self._nome} | Preço: {self._preco} | Tipo: {self._tipo} | Tamanho: {self.tamanho} | Descrição: {self.descricao}'
+        
+    def aplicar_desconto(self):
+        self._preco -= (self._preco * 0.1)
